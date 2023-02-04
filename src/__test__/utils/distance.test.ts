54fbb1400f2bd6f65617670b28c8a4c1d21633ba
Mykyta Monokov <Mykyta_Monokov@neimanmarcus.com>
2023-02-04 18:59:21 +0200

    Adjust code style

--- src/__test__/utils/distance.test.ts
+++ src/__test__/utils/distance.test.ts
@@ -22,22 +22,22 @@ describe('distance test', () => {
       threshold)).toBeTruthy();
   });
 
-    test('distance::isNear === false', () => {
-        expect(isNear(
-          { lat: 49.223839, lng: 28.435827 },
-          { lat: 49.230562,lng: 28.435197 },
-          threshold)).toBeFalsy();
-        expect(isNear(
-          { lat: 49.223839, lng: 28.435827 },
-          { lat: 49.225039,lng: 28.420290 },
-          threshold)).toBeFalsy();
-        expect(isNear(
-          { lat: 49.223839, lng: 28.435827 },
-          { lat: 49.233683,lng: 28.465075 },
-          threshold)).toBeFalsy();
-        expect(isNear(
-          { lat: 49.223839, lng: 28.435827 },
-          { lat: 49.217676,lng: 28.438543 },
-          threshold)).toBeFalsy();
-    });
+  test('distance::isNear === false', () => {
+    expect(isNear(
+      { lat: 49.223839, lng: 28.435827 },
+      { lat: 49.230562, lng: 28.435197 },
+      threshold)).toBeFalsy();
+    expect(isNear(
+      { lat: 49.223839, lng: 28.435827 },
+      { lat: 49.225039, lng: 28.420290 },
+      threshold)).toBeFalsy();
+    expect(isNear(
+      { lat: 49.223839, lng: 28.435827 },
+      { lat: 49.233683, lng: 28.465075 },
+      threshold)).toBeFalsy();
+    expect(isNear(
+      { lat: 49.223839, lng: 28.435827 },
+      { lat: 49.217676, lng: 28.438543 },
+      threshold)).toBeFalsy();
+  });
 });
